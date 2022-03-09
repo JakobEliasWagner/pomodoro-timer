423bd9f2858a766069ebf545014c8a60edc6e318
Jakob Wagner <jakob.e.wagner@tum.de>
2022-03-09 16:12:37 +0100

    setup project

---
+++ source/UI/ui_test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jakob on 09.03.22.
+//
+
+#include "ui_test.h"
---
+++ source/UI/ui_test.h
@@ -0,0 +1,10 @@
+//
+// Created by jakob on 09.03.22.
+//
+
+#ifndef POMODORO_TIMER_UI_TEST_H
+#define POMODORO_TIMER_UI_TEST_H
+
+class ui_test {};
+
+#endif // POMODORO_TIMER_UI_TEST_H
